78e50354f6ad55f720af884d0cab1c65fa11adf6
Julian Yap <julianokyap@gmail.com>
2022-02-15 10:48:15 -0800

    Remove NOTIFICATIONS_ONBOARDING_MORE button

--- src/features/NotificationsPanel/components/OnboardingPleaseUnderstandNotification.tsx
+++ src/features/NotificationsPanel/components/OnboardingPleaseUnderstandNotification.tsx
@@ -1,35 +1,15 @@
-import { Button } from '@mycrypto/ui';
 import styled from 'styled-components';
 
 import cantAccessIcon from '@assets/images/onboarding/icn-cant-access.svg';
 import cantFreezeIcon from '@assets/images/onboarding/icn-cant-freeze.svg';
 import cantModifyIcon from '@assets/images/onboarding/icn-cant-modify.svg';
 import cantReverseIcon from '@assets/images/onboarding/icn-cant-reverse.svg';
-import { getKBHelpArticle, KB_HELP_ARTICLE } from '@config';
 import { BREAK_POINTS } from '@theme';
 import translate, { translateRaw } from '@translations';
 
 import NotificationWrapper from './NotificationWrapper';
 
-const { SCREEN_XS, SCREEN_MD } = BREAK_POINTS;
-
-const ResourceLink = styled.a`
-  @media (min-width: ${SCREEN_MD}) {
-    margin-top: 60px;
-  }
-`;
-
-const ResourceItem = styled(Button)`
-  width: 200px;
-  padding-left: 0px;
-  padding-right: 0px;
-  font-weight: normal;
-  font-size: 17px;
-
-  @media (max-width: ${SCREEN_XS}) {
-    font-size: 15px;
-  }
-`;
+const { SCREEN_MD } = BREAK_POINTS;
 
 const Content = styled.div`
   display: flex;
@@ -65,15 +45,7 @@ export default function OnboardingPleaseUnderstandNotification() {
       alignCenterOnSmallScreen={true}
       tagline={translate('NOTIFICATIONS_ONBOARDING_TAGLINE')}
       title={`${translateRaw('NOTIFICATIONS_ONBOARDING_PLEASE_UNDERSTAND_TITLE')}:`}
-      resources={
-        <ResourceLink
-          href={getKBHelpArticle(KB_HELP_ARTICLE.HOME)}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <ResourceItem secondary={true}>{translate('NOTIFICATIONS_ONBOARDING_MORE')}</ResourceItem>
-        </ResourceLink>
-      }
+      resources={<></>}
     >
       <Content>
         <TipItem>
